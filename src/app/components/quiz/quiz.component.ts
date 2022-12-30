47dc287bdf30b45e1e49d02daf9c146594f9ba32
harryazawa <victorlbamorim@gmail.com>
2022-12-30 15:41:05 -0300

    feat: added business rules

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json"
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
+  title:string = ""
+
+  questions:any
+  selectedQuestion:any
+
+  answers:string[] = []
+  selectedAnswer:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false
+      this.title = quiz_questions.title
+
+      this.questions = quiz_questions.questions
+      this.selectedQuestion = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
   }
 
+  playerChoice(value:string) {
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.selectedQuestion = this.questions[this.questionIndex]
+    }else{
+      this.finished = true
+    }
+  }
 }
